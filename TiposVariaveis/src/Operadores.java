b4c9914325f876daeb1e58c5f362cb479f39ce54
duprof10 <duprofxadrez@yahoo.com.br>
2024-06-17 10:40:14 -0300

    commit novo

--- TiposVariaveis/src/Operadores.java
+++ TiposVariaveis/src/Operadores.java
@@ -1,3 +1,32 @@
 public class Operadores {
+  
+   public static void main(String[] args) {
+
+      int numero1 = 1;
+      int numero2 = 2;
+
+      if (numero1 < numero2) {
+         System.out.println("a nossa condição é verdadeira");
+      }
+
+      //boolean simNao = numero1 > numero2; // a expressão ( != direfente ) (== igual) (> maior) ( < menor)     
+       //System.out.println("Número 1 é maior do número 2? " + simNao);
+    
+      //int a, b;
+      //a = 6;
+      //b = 6;
+
+      //String resultado ="";
+      //if (a==b) 
+      //resultado = "verdadeiro";
+      //else
+      //resultado = "falso" ;
+
+      // String resultado = a==b ? "verdadeiro" : "falso"; // isso simplifica o algoritmo acima e economiza
+      // int resultado = a==b ? 1 : 0;  //o booLean representa == e pode escolher entre 0 e 1 nesse caso
+
+      //System.out.println(resultado);
+   
+   }
     
 }
